260c24a9a090229b0725e3a9eac0c51db56e5148
Chris <chrisphilholt@gmail.com>
2022-07-05 16:16:29 +0100

    final commit

---
+++ index.js
@@ -0,0 +1,7 @@
+alert("You are our 1000th visitor!!!! You have won an Apple 2!!! Claim your free prize now!!! \n Please visit: www.ClaimMyApple2UK.com")
+
+alert("You are our 1000th visitor!!!! You have won an Apple 2!!! Claim your free prize now!!! \n Please visit: www.ClaimMyApple2UK.com")
+
+alert("You are our 1000th visitor!!!! You have won an Apple 2!!! Claim your free prize now!!! \n Please visit: www.ClaimMyApple2UK.com")
+alert("You are our 1000th visitor!!!! You have won an Apple 2!!! Claim your free prize now!!! \n Please visit: www.ClaimMyApple2UK.com")
+alert("You are our 1000th visitor!!!! You have won an Apple 2!!! Claim your free prize now!!! \n Please visit: www.ClaimMyApple2UK.com")
